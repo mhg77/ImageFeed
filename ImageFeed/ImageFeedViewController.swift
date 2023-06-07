6630b0f14fec24a17489725ea5989630ffb195c8
mhg77 <hashier@yandex.ru>
2023-06-07 20:15:02 +0300

    add extension

--- ImageFeed/ImageFeedViewController.swift
+++ ImageFeed/ImageFeedViewController.swift
@@ -8,14 +8,29 @@
 import UIKit
 
 class ImageFeedViewController: UIViewController {
-
+    
     @IBOutlet private var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ImageFeedViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ImageFeedViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
