347c47bbea3c4d4e6b93096b1b6715e9afc36cb6
mhg77 <hashier@yandex.ru>
2023-06-07 22:44:58 +0300

    add custom cell

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -5,8 +5,12 @@
 //  Created by Михаил Асаилов on 07.06.2023.
 //
 
-import Foundation
+import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    @IBOutlet var cellImageView: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
     
+    static let reuseIdentifier = "ImagesListCell"
 }
