0365a392a16023d0aac1d9bf969c837b75be1d79
mhg77 <hashier@yandex.ru>
2023-06-25 17:11:58 +0300

    add ProfileViewController

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -12,6 +12,5 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet var likeButton: UIButton!
     @IBOutlet var dateLabel: UILabel!
     
-    
     static let reuseIdentifier = "ImagesListCell"
 }
