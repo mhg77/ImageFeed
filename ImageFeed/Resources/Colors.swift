33df5e4d045f8dc423251c5fcd8ed247c122cd07
mhg77 <hashier@yandex.ru>
2023-07-05 01:53:39 +0300

    add Colors file

---
+++ ImageFeed/Resources/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  ImageFeed
+//
+//  Created by Михаил Асаилов on 05.07.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let ypBackground = UIColor(named: "YP Background (iOS)")
+    static let ypBlack = UIColor(named: "YP Black (iOS)")
+    static let ypBlue = UIColor(named: "YP Blue (iOS)")
+    static let ypGray = UIColor(named: "YP Gray (iOS)")
+    static let ypRed = UIColor(named: "YP Red (iOS)")
+    static let ypWhite50 = UIColor(named: "YP White (Alpha 50) (iOS)")
+    static let ypWhite = UIColor(named: "YP White (iOS)")
+}
