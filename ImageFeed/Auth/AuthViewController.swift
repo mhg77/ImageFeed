93db60c90a51381fc1e20fe8608036e04e92163b
mhg77 <hashier@yandex.ru>
2023-07-11 22:51:42 +0300

    add WebView

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Михаил Асаилов on 11.07.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Михаил Асаилов on 11.07.2023.
+//
+
+import UIKit
+
+final class WebViewViewController: UIViewController {
+    
+}
