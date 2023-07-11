8b56b0466dd49576667e776ff6a2b56ed71b3198
mhg77 <hashier@yandex.ru>
2023-07-12 00:01:02 +0300

    add url components

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -12,6 +12,22 @@ final class WebViewViewController: UIViewController {
     @IBOutlet private var webView: WKWebView!
     @IBOutlet private var backButton: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var urlComponents = URLComponents(string: unsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+           URLQueryItem(name: "client_id", value: accessKey),
+           URLQueryItem(name: "redirect_uri", value: redirectURI),
+           URLQueryItem(name: "response_type", value: "code"),
+           URLQueryItem(name: "scope", value: accessScope)
+         ]
+         let url = urlComponents.url!
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
     @IBAction func didTapBackButton(_ sender: Any) {
     }
     
